d59df79231bbce4d9b92ff6ce9d5df6c78dc6029
Mohtashim-1 <shoaibmohtashim973@gmail.com>
2024-07-31 13:55:15 +0000

    feat: add qty method added

--- buying_addon/buying_addon/doctype/purchase_order/purchase_order.js
+++ buying_addon/buying_addon/doctype/purchase_order/purchase_order.js
@@ -32,3 +32,110 @@ frappe.ui.form.on("Purchase Order Item", {
 		});
 	}
 })
+
+frappe.ui.form.on("Purchase Order Item", {
+	rate: function(frm, cdt, cdn) {
+	  set_po_rate(frm, cdt, cdn);
+	  
+	},
+	qty: function(frm, cdt, cdn) {
+	  set_po_rate(frm, cdt, cdn);
+	  
+	},  
+  });
+  function set_po_rate(frm, cdt, cdn){
+	  var d = locals[cdt][cdn];
+	
+	frappe.model.set_value(d.doctype, d.name, 'po_rate', d.rate);
+	frappe.model.set_value(d.doctype, d.name, 'po_amount', d.rate*d.qty);
+  }
+  
+  
+  
+frappe.ui.form.on("Purchase Order Item",{
+		rate: function (frm,cdt,cdn){
+			set_po_rate(frm,cdt,cdn)
+		},
+		qty:function(frm,cdt,cdn){
+			set_po_rate(frm,cdt,cdn)
+		}
+	
+})
+
+function set_po_rate(frm,cdt,cdn){
+	var d = locals[cdt][cdn]
+	frappe.model.set_value(d.doctype, d.name, "custom_po_rate",d.rate )
+	frappe.model.set_value(d.doctype, d.name, "custom_po_amount", (d.rate * d.qty) )
+}
+
+frappe.ui.form.on('Purchase Order',{
+	before_submit:function (frm){
+		if(frm.doc.total<=0){
+			frappe.throw(
+			msg = "Purchase Order Document can not be submit without Rate"
+			);
+			frappe.validated = false
+		}
+		frm.set_value('custom_po_total',frm.doc.total)
+	}
+})
+
+// frappe.ui.form.on('Purchase Order',{
+// 	before_submit:function(frm){
+// 		$.each(frm.doc.items, function(i, d){
+// 			if(d.custom_temp_qty == 0){
+// 				d.custom_temp_qty = d.qty
+// 				d.custom_real_qty = d.qty
+// 				frappe.msgprint('1')
+// 			}
+// 		})
+// 	}
+// })
+frappe.ui.form.on('Purchase Order',{
+	refresh:function(frm){
+		$.each(frm.doc.items, function(i, d){
+			if(d.custom_temp_qty == 0){
+				d.custom_temp_qty = d.qty
+				d.custom_real_qty = d.qty
+			}
+		});
+		frm.refresh_field('items');
+		cur_frm.refresh_field("items")
+	}
+})
+
+
+frappe.ui.form.on('Purchase Order Item',{
+	custom_qty_:function(frm,cdt,cdn){
+		increase_qty(frm,cdt,cdn)
+	},
+	qty:function(frm,cdt,cdn){
+		var d = locals[cdt][cdn]
+		if(d.custom_temp_qty==0){
+			d.custom_temp_qty = d.qty
+		}
+		var new_qty = ((d.custom_temp_qty-d.qty) *100 )/ d.custom_temp_qty
+		console.log(new_qty)
+		d.custom_qty_ = new_qty
+		d.custom_real_qty = d.custom_temp_qty
+		cur_frm.refresh_field('items')
+	
+	}
+})
+
+function increase_qty(frm,cdt,cdn){
+	var d = locals[cdt][cdn]
+	if(d.custom_temp_qty==0){
+		d.custom_temp_qty = d.qty
+	}
+	var act_qty = d.custom_temp_qty
+	var add_per = (d.custom_qty_*d.qty)/100
+	var new_qty = act_qty+add_per
+	d.custom_real_qty= act_qty
+	if(d.uom == "KG" || d.uom == "METER"){
+		d.qty = new_qty
+	}
+	else{
+		d.qty = Math.round(new_qty)
+	}
+}
