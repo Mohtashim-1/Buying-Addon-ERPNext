4e80bb8eb2094c7530afcb5c8d3b3fe80c8729ad
Mohtashim-1 <shoaibmohtashin973@gmail.com>
2025-07-31 15:39:28 +0500

    chore: po update

--- buying_addon/hooks.py
+++ buying_addon/hooks.py
@@ -28,7 +28,9 @@ app_license = "mit"
 # page_js = {"page" : "public/js/file.js"}
 
 # include js in doctype views
-doctype_js = {"Purchase Order" : "buying_addon/doctype/purchase_order/purchase_order.js"}
+doctype_js = {
+    "Purchase Order" : "buying_addon/doctype/purchase_order/purchase_order.js",
+}
 # doctype_js = {"doctype" : "public/js/doctype.js"}
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
